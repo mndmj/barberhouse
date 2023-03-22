9d217f1eb6a6b9047b8b66595b783a3a26d5e2e0
mndmj <nasrullah.03.arul@gmail.com>
2023-03-23 02:33:06 +0700

    add transaksi

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -3,16 +3,19 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ModelDetailTransaksi;
 use App\Models\ModelTransaksi;
 
 class Transaksi extends BaseController
 {
+    private $ModelDetailTransaksi = null;
     private $ModelTransaksi = null;
     private $db = null;
 
     public function __construct()
     {
         $this->ModelTransaksi = new ModelTransaksi();
+        $this->ModelDetailTransaksi = new ModelDetailTransaksi();
         $this->db = \config\Database::connect();
         helper('form');
     }
@@ -22,6 +25,11 @@ class Transaksi extends BaseController
         $data = [
             'title' => 'Barberhouse',
             'subtitle' => 'Transaksi',
+            'bb' => $this->db->table('tbl_bb')->where('id_bb', session('data_user')['id_bb'])->get()->getResultArray()[0],
+            'keranjang' => $this->ModelDetailTransaksi
+                ->join('tbl_menu', 'tbl_menu.id_menu=tbl_detail_transaksi.id_menu')
+                ->join('tbl_transaksi', 'tbl_transaksi.id_transaksi=tbl_detail_transaksi.id_transaksi')
+                ->where('id_antrian', session('id_antrian'))->findAll(),
         ];
         return view('admin/view_transaksi', $data);
     }
--- app/Views/admin/view_transaksi.php
+++ app/Views/admin/view_transaksi.php
@@ -1,51 +1,9 @@
 <?= $this->extend('template/t_admin'); ?>
 <?= $this->section('content'); ?>
 
+
 <div class="row">
-    <div class="col-sm-5 ms-auto">
-        <div class="card card-dark">
-            <h5 class="card-header">Data Antrian</h5>
-            <div class="card-body">
-                <table>
-                    <tr>
-                        <td>Nama Barbershop</td>
-                        <td class="px-3">:</td>
-                        <td><?= $bb['nama_bb'] ?></td>
-                    </tr>
-                    <tr>
-                        <td>Tanggal</td>
-                        <td class="px-3">:</td>
-                        <td><?= date('d-m-Y') ?></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-5 me-auto">
-        <div class="card card-dark">
-            <h5 class="card-header">Daftar Menu</h5>
-            <div class="card-body">
-                <label>Daftar Menu</label>
-                <form action="<?= base_url('') ?>" method="post" class="d-flex">
-                    <select class="form-select mb-3" id="" name="pilih_menu">
-                        <option selected>Pilih Menu</option>
-                        <?php foreach () { ?>
-                            <option value=""></option>
-                        <?php } ?>
-                    </select>
-                    <div class="input-group ms-3" style="width: 200px;">
-                        <input class="form-control" type="number" placeholder="Jumlah" name="jumlah_dt">
-                        <button class="btn btn-primary" style="height:fit-content">
-                            <i class="fa-solid fa-plus"></i>
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-sm-10 mx-auto">
+    <div class="col-sm-12">
         <div class="card card-dark">
             <div class="card-header">
                 <div class="row">
@@ -54,7 +12,7 @@
                     </div>
                     <div class="col-6">
                         <div class="float-right bg-danger p-2 rounded">
-                            <h6 class="m-0 p-0">Total Transaksi = Rp. 50.000</h6>
+                            <h6 class="m-0 p-0">Total Transaksi = </h6>
                         </div>
                     </div>
                 </div>
@@ -82,9 +40,6 @@
                                 <td><?= $value['jumlah_dt'] ?></td>
                                 <td><?= $value['harga_dt'] * $value['jumlah_dt'] ?></td>
                                 <td>
-                                    <button class="btn btn-sm btn-flat btn-warning" data-bs-toggle="modal" data-bs-target="#edit">
-                                        <i class="fas fa-pen"></i>
-                                    </button>
                                     <button class="btn btn-sm btn-flat btn-danger" data-bs-toggle="modal" data-bs-target="#delete">
                                         <i class="fas fa-trash"></i>
                                     </button>
@@ -98,8 +53,43 @@
     </div>
 </div>
 
+<!-- Modal Add -->
+<div class="modal fade" id="add">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h4 class="modal-title">Add Menu</h4>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body"><label>Daftar Menu</label>
+                <form action="<?= base_url('') ?>" method="post" class="d-flex">
+                    <select class="form-select mb-3" id="" name="pilih_menu" style="width: 300px;">
+                        <option selected>Pilih Menu</option>
+                        <?php  ?>
+                        <option value=""></option>
+                        <?php  ?>
+                    </select>
+                    <div class="input-group ms-1" style="width: 200px;">
+                        <input class="form-control" type="number" placeholder="Jumlah" name="jumlah_dt">
+                        <button class="btn btn-primary" style="height:fit-content">
+                            <i class="fa-solid fa-plus"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
     $('#keranjang').DataTable();
+    $(document).ready(function() {
+        $('#keranjang_filter').empty();
+        let btn_add = $('<button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#add"></button>').append('<i class="fas fa-plus"></i> Add');
+        $('#keranjang_filter').append(btn_add);
+    });
 </script>
 
 <?= $this->endSection(); ?>
